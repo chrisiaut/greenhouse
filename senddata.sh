b609a8b55659de5b60eed8afb965d4bb41efaeca
Christian Haschek <christian@haschek.at>
2016-12-21 02:05:37 +0100

    added more sensors

--- senddata.sh
+++ senddata.sh
@@ -6,11 +6,14 @@ cd /root/greenhouse
 LIGHT="$(./light_status.sh)"
 TEMPERATURE=$(echo "scale=2; $(./get_temp.sh)/1000" | bc)
 CONDENSATION="$(python get_analog.py 0)"
+HUMIDITY=$(echo "scale=2; ($(python get_analog.py 2)/1024)*100" | bc)
 
 echo "light status: ${LIGHT}";
 echo "Temperature: ${TEMPERATURE}";
 echo "Condensation: ${CONDENSATION}";
+echo "Humidity: ${HUMIDITY}";
 
 echo "light value=${LIGHT}"> /dev/udp/192.168.1.117/8070
 echo "temperature value=${TEMPERATURE}"> /dev/udp/192.168.1.117/8070
 echo "condensation value=${CONDENSATION}"> /dev/udp/192.168.1.117/8070
+echo "humidity value=${HUMIDITY}"> /dev/udp/192.168.1.117/8070
